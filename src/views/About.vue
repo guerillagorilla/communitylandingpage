24fb1f248b60af714c87cb59c8adf7af9306c324
Andrew <32229265+aselee@users.noreply.github.com>
2018-10-22 15:55:11 -0500

    DevLee testing about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>This is DevLee testing real quick</h2>
+    <h3>Testing one more time, creating branch</h3>
   </div>
 </template>
