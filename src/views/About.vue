018779b041b571c79d28523e337868f16371295f
guerillagorilla <novajeeper@gmail.com>
2018-10-25 12:24:17 -0400

    Testing two

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,5 +3,6 @@
     <h1>This is an about page</h1>
     <h2>This is DevLee testing real quick</h2>
     <h3>Testing one more time, creating branch</h3>
+    <h3>One last test...</h3>
   </div>
 </template>
